1945900c6c6ff3176b3a3186ff968a3e81c67f73
Fabio Cavaleri <fabioc.1100@gmail.com>
2022-01-22 21:01:27 +0100

    Fix Delete Preferiti

--- app/src/main/java/it/unimib/ginlemons/ui/ricette/RicettePreferitiFragment.java
+++ app/src/main/java/it/unimib/ginlemons/ui/ricette/RicettePreferitiFragment.java
@@ -131,7 +131,10 @@ public class RicettePreferitiFragment extends Fragment {
 
                 final int position = viewHolder.getAdapterPosition();
                 Ricetta ricetta = preferitiRicetteRecyclerviewAdapter.getList().get(position);
-                preferitiRicetteRecyclerviewAdapter.getList().remove(position);
+
+                if(ricettePreferitiList.size() != preferitiRicetteRecyclerviewAdapter.getList().size())
+                    preferitiRicetteRecyclerviewAdapter.getList().remove(position);
+
                 // remove on FireBase
                 ricettePreferitiList.remove(position);
                 RicettaHelper ricettaHelper = new RicettaHelper(ricetta.getId(), ricetta.getName(), ricetta.getType());
